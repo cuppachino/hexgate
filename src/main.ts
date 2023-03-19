ba9f59d6d28a825954b8de8ef13bedb197255755
cuppachino <cuppachino.dev@gmail.com>
2023-03-19 17:19:26 -0500

    chore: export auth and associate types from main

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,3 @@
-export {}
+export { auth } from 'hexgate/modules/auth/index.js'
+export type { Credentials, AuthToken, AuthOptions } from 'hexgate/types/tokens.js'
+export type { ErrorKind } from 'hexgate/types/errors.js'
