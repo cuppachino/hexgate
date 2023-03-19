48fbe053476caf865c09729e50e4dfd985412e0d
cuppachino <cuppachino.dev@gmail.com>
2023-03-19 17:09:41 -0500

    chore: update jsdoc error comments

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -5,7 +5,7 @@ import { getTokens } from './tokens.js'
 /**
  * Execute a shell command and extract `AuthTokens` from the League of Legends client process.
  * @throws `InvalidPlatformError` if the platform is not Windows or Linux.
- * @throws `MissingClientError` if any required token is missing.
+ * @throws `MissingClientError` if any required token is missing, which indicates that the client is not running or cannot be found.
  *
  * @returns `Credentials`
  */
