900598345ddf5176f0c1abd50a417afcf44b6a67
cuppachino <cuppachino.dev@gmail.com>
2023-03-19 17:03:11 -0500

    feat: type AuthTokens and AuthOptions

--- src/types/tokens.ts
+++ src/types/tokens.ts
@@ -1,4 +1,5 @@
 import type { PartialSome } from '@cuppachino/type-space'
+import type { CERTIFICATE } from 'hexgate/constants/certificate.js'
 import type { AUTH_TOKENS } from 'hexgate/modules/auth/tokens.js'
 
 /**
@@ -7,7 +8,27 @@ import type { AUTH_TOKENS } from 'hexgate/modules/auth/tokens.js'
 export type AuthToken = typeof AUTH_TOKENS[number]
 
 /**
- * A map of token IDs to their types.
+ * The tokens extracted from the client process.
+ *
+ * The certificate is not extracted from the client. It is either provided by the user, or defaults to the public certificate.
+ */
+export type AuthTokens = Omit<Credentials, 'certificate'>
+
+/**
+ * Options for the `auth` function.
+ *
+ * By default, the public certificate will be used, but you may provide your own.
+ *
+ * Alternatively, you can force "unsafe" authentication by explicitly setting `certificate` to `undefined`.
+ */
+export type AuthOptions<Cert extends string | undefined = typeof CERTIFICATE> = {
+  certificate?: Cert
+}
+
+/**
+ * Validated credentials ready to be used for authentication.
+ *
+ * Without a certificate, "unsafe" authentication will be used.
  */
 export type Credentials = PartialSome<
   {
