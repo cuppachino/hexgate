1310bb118eea02b262026e763633be19f8a76d8d
cuppachino <cuppachino.dev@gmail.com>
2023-03-20 10:57:40 -0500

    feat: create headers from remoteAuthToken

---
+++ src/modules/auth/headers.ts
@@ -0,0 +1,10 @@
+import type { Subset } from '@cuppachino/type-space'
+import type { Credentials } from '../../types/tokens.js'
+import type { HeadersInit } from 'node-fetch'
+
+export function createHeaders(credentials: Subset<Credentials, 'remotingAuthToken'>) {
+  const password = typeof credentials === 'string' ? credentials : credentials.remotingAuthToken
+  return {
+    Authorization: 'Basic ' + Buffer.from(`riot:${password}`).toString('base64')
+  } satisfies HeadersInit
+}
