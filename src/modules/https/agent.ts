e5122cf7c75de323352549dcf963f35a7b33f20d
cuppachino <cuppachino.dev@gmail.com>
2023-03-20 13:58:04 -0500

    feat: add createRequestOptions

--- src/modules/https/agent.ts
+++ src/modules/https/agent.ts
@@ -2,10 +2,18 @@ import type { Subset } from '@cuppachino/type-space'
 import type { Credentials } from '../../types/tokens.js'
 import https from 'node:https'
 
-export class HttpsAgent extends https.Agent {
-  constructor(credential: Subset<Credentials, 'certificate'>, options?: https.AgentOptions) {
+/**
+ * A helper class for instantiating an https.Agent from a certificate string or `Credentials` object.
+ *
+ * @param credential - Either a certificate string or an object with a certificate property (e.g. `Credentials`)
+ * @param options - Additional, non-essential, options to pass to the https.Agent constructor.
+ */
+export class HttpsAgent<Cert extends Subset<Credentials, 'certificate'>> extends https.Agent {
+  constructor(credential: Cert, options?: https.AgentOptions) {
     const certificate = typeof credential === 'string' ? credential : credential?.certificate
     super({
+      keepAlive: true,
+      maxSockets: 25,
       ...options,
       rejectUnauthorized: !!certificate,
       ca: options?.ca ?? certificate
