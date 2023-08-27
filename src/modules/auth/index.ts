3be34c0d34db39613c30fa4bc2e2f29007502db3
cuppachino <cuppachino.dev@gmail.com>
2023-08-26 20:03:46 -0500

    fix `auth` return type when passed as callback fn

--- src/modules/auth/index.ts
+++ src/modules/auth/index.ts
@@ -9,32 +9,38 @@ import { getTokens } from './tokens.js'
  *
  * @returns `Promise<Credentials>`
  */
+export async function auth(): Promise<Credentials>
+/**
+ * Execute a shell command and extract `AuthTokens` from the League of Legends client process.
+ * @throws `InvalidPlatformError` if the platform is not Windows, MacOS, or Linux.
+ * @throws `MissingClientError` if any required token is missing, which indicates that the client is not running or cannot be found.
+ *
+ * @returns `Promise<Credentials>`
+ */
 export async function auth(
   options?: AuthOptions & { all?: undefined }
 ): Promise<Credentials>
 /**
- * Execute a shell command and extract `AuthTokens` from all League of Legends client processes.
+ * Execute a shell command and extract `AuthTokens` from the League of Legends client process.
  * @throws `InvalidPlatformError` if the platform is not Windows, MacOS, or Linux.
- * @throws `MissingClientError` if any required token is missing, which indicates that the client is not running or cannot be found.
+ * @throws `MissingClientError` if any required token is missing from all found clients, which indicates that the client is not running, cannot be found, or is being modified by another process.
  *
  * @returns `Promise<Credentials[]>`
  */
 export async function auth(
-  options?: AuthOptions & {
-    all: true
-  }
+  options?: AuthOptions & { all: true }
 ): Promise<Credentials[]>
 export async function auth(
-  options?: AuthOptions & { all?: true }
+  options?: AuthOptions & ({ all: true } | { all?: undefined })
 ): Promise<Credentials | Credentials[]> {
   const tokenList = (await getTokens()).map((tokens) => ({
     ...tokens,
     certificate: options?.certificate || CERTIFICATE
   }))
 
-  if (options?.all === true) {
+  if (!options?.all) {
+    return tokenList[0]
+  } else {
     return tokenList
   }
-
-  return tokenList[0]
 }
